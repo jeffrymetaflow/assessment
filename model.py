404c101db712a6c3b40a7b7b7741f3d881175316
jeffrymetaflow <jeff.smith@cloudsoda.io>
2025-04-16 14:14:16 -0500

    Update model.py

--- model.py
+++ model.py
@@ -93,6 +93,13 @@ By adopting an AI-optimized IT revenue framework, <Client Name> can align IT ope
             .replace("### ", "") \
             .replace("---", "
 ----------------------
+") \
+            .replace("  ", "
+") \
+            .replace("## ", "") \
+            .replace("### ", "") \
+            .replace("---", "
+----------------------
 ")
         pdf.chapter_body(cleaned_summary)
 File "/mount/src/itrm/model.py", line 88
