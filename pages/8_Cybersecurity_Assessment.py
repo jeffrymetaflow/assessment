96500784c94cba3cd4dc6907bc7fd8e73810cdf9
jeffrymetaflow <jeff.smith@cloudsoda.io>
2025-04-23 10:04:41 -0500

    Update 8_Cybersecurity_Assessment.py

--- pages/8_Cybersecurity_Assessment.py
+++ pages/8_Cybersecurity_Assessment.py
@@ -12,7 +12,7 @@ st.title("📈 Cybersecurity_Assessment")
 section = "🧭 Cybersecurity Assessment"  # Define the variable
 
 # Cybersecurity Assessment Tab
-elif section == "🔐 Cybersecurity Assessment":
+if section == "🔐 Cybersecurity Assessment":
     st.title("🔐 Cybersecurity Maturity Assessment")
     st.markdown("For more details, visit the [NIST Cybersecurity Framework website](https://www.nist.gov/).")
 
