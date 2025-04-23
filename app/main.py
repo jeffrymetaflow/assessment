1c039b9977e270e08f56a218b79fed32d3553bfa
jeffrymetaflow <jeff.smith@cloudsoda.io>
2025-04-22 19:27:00 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
-main.py
-/controller/controller.py
-/pages/1_Component_Mapping.py
-/assets/
+import streamlit as st
+from controller.controller import ITRMController
+
+if "controller" not in st.session_state:
+    st.session_state.controller = ITRMController()
+
+st.title("💡 ITRM Platform")
+st.write("Welcome! Use the tabs at the left to explore your IT strategy.")
