e7185feb65b71ee1eee7276ce5d798777d8d4ec9
jeffrymetaflow <jeff.smith@cloudsoda.io>
2025-05-02 15:26:36 -0500

    Update 10_Strategic_Roadmap.py

--- pages/10_Strategic_Roadmap.py
+++ pages/10_Strategic_Roadmap.py
@@ -8,6 +8,8 @@ import numpy as np
 from utils.bootstrap import page_bootstrap
 from utils.session_state import initialize_session
 initialize_session()
+from utils.auth import enforce_login
+enforce_login()
 
 st.set_page_config(page_title="Strategic_Roadmap", layout="wide")
 st.title("📈 Strategic_Roadmap")
