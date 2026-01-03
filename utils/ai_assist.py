adbecef35a259cc5bf65b824d98c7ea22d698f71
jeffrymetaflow <jeff.smith@cloudsoda.io>
2026-01-03 12:58:19 -0600

    Update import for AgentExecutor from langchain_core

--- utils/ai_assist.py
+++ utils/ai_assist.py
@@ -11,8 +11,7 @@ from langchain_community.tools.tavily_search.tool import TavilySearchResults
 from langchain_core.callbacks.manager import CallbackManagerForToolRun
 from langchain_community.tools import Tool
 from langchain_community.agent_toolkits import create_openai_functions_agent
-from langchain.agents import AgentExecutor
-
+from langchain_core.agents import AgentExecutor
 from utils.intent_classifier import classify_intent
 from postgrest.exceptions import APIError
 from utils.supabase_client import supabase
